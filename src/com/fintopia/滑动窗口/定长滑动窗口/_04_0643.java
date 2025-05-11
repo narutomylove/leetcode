d7a037e65730dc1a315435925a8b4038ef0af267
ziminglu <ziminglu@fintopia.tech>
2025-05-11 17:37:33 +0800

    [20250511][滑动窗口2]

--- src/com/fintopia/滑动窗口/定长滑动窗口/_04_0643.java
+++ src/com/fintopia/滑动窗口/定长滑动窗口/_04_0643.java
@@ -1,4 +1,18 @@
 package com.fintopia.滑动窗口.定长滑动窗口;
 
 public class _04_0643 {
+
+  public double findMaxAverage(int[] nums, int k) {
+    int maxSum = 0;
+    for (int i = 0; i < k; i++) {
+      maxSum += nums[i];
+    }
+    int lastSum = maxSum;
+    for (int i = k; i < nums.length; i++) {
+      int tempSum = lastSum + nums[i] - nums[i - k];
+      maxSum = Math.max(maxSum, tempSum);
+      lastSum = tempSum;
+    }
+    return (double) maxSum / k;
+  }
 }
